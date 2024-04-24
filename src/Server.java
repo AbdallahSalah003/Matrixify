36488ea512a1a3495b8ad7521867ab27d6d58d4b
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2024-04-25 00:39:48 +0200

    Override doGet() and take input matrix

--- src/Server.java
+++ src/Server.java
@@ -2,5 +2,16 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 public class Server extends HttpServlet {
-
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        String input = request.getParameter("matrix");
+        String[] inputArr = input.split(", ");
+        int[] convArr = new int[inputArr.length];
+        for (int i = 0; i < inputArr.length; i++) {
+            convArr[i] = Integer.parseInt(inputArr[i]);
+        }
+        int length = (int)Math.sqrt(inputArr.length);
+        Matrix matrix = new Matrix(length);
+        matrix.SetNumbers(convArr);
+    }
 }
