6a29b59c1e6c8851fb521b0a64c3ceab67d8bf1e
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2024-04-25 00:02:27 +0200

    Add class Matrix with its methods

---
+++ src/Matrix.java
@@ -0,0 +1,29 @@
+public class Matrix {
+    protected  int N;
+    protected int[][] matrix;
+    Matrix(int dim) {
+        this.N = dim;
+        this.matrix = new int[N][N];
+    }
+    public boolean SetNumbers(int[] input) {
+        if(input.length < this.N*this.N) {
+            return false;
+        }
+        int ind = 0;
+        for(int i=0; i< this.N; ++i) {
+            for(int j=0; j< this.N; ++j) {
+                matrix[i][j] = input[ind++];
+            }
+        }
+        return true;
+    }
+    public void Transpose() {
+        int[][] tmp=this.matrix;
+        this.matrix=new int[N][N];
+        for(int i=0; i<N; ++i) {
+            for(int j=0; j<N; ++j) {
+                matrix[i][j]=tmp[j][i];
+            }
+        }
+    }
+}
