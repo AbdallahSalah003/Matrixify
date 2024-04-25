bd7b1afc6823f8a4f18de27c6afb55e5b280560c
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2024-04-25 12:03:40 +0200

    Send redirect html error pages

--- src/Server.java
+++ src/Server.java
@@ -9,44 +9,32 @@ public class Server extends HttpServlet {
         String input = request.getParameter("matrix");
         String page;
         if(input.isEmpty()) {
-            page = "<!DOCTYPE html>\n" +
-                    "<html>\n" +
-                    "<body bgcolor=\"#f0f0f0\" align=\"center\" >\n" +
-                    "<h1 align=\"center\"> Matrix Operations </h1>\n" +
-                    "\n" +
-                    "<textarea rows=\"12\" cols=\"40\" name=\"matrix\" form=\"MatrixForm\" placeholder=\"Enter a Square Matrix ...\"></textarea>\n" +
-                    "<br>\n" +
-                    "<br>\n" +
-                    "\n" +
-                    "<form action=\"MatrixForm\" method=\"GET\" id=\"MatrixForm\">\n" +
-                    "    <input type=\"checkbox\" name=\"Transpose\" /> Transpose\n" +
-                    "    <input type=\"checkbox\" name=\"IsIdentity\"  /> Is Identity\n" +
-                    "    <br>\n" +
-                    "    <br>\n" +
-                    "    <p style=\"color: red\">Please insert a square matrix</p>\n" +
-                    "    <br>\n" +
-                    "    <input type=\"submit\" value=\"Calculate\" />\n" +
-                    "</form>\n" +
-                    "\n" +
-                    "</body>\n" +
-                    "</html>";
+            page = "/errorNoInput.html";
+            response.sendRedirect(page);
         }
         else {
-            String[] inputArr = input.split(" ");
+            String[] inputArr = input.split("\\s+");
             int[] convArr = new int[inputArr.length];
             for (int i = 0; i < inputArr.length; i++) {
                 convArr[i] = Integer.parseInt(inputArr[i]);
             }
             int length = (int)Math.sqrt(inputArr.length);
-            Matrix matrix = new Matrix(length);
-            matrix.SetNumbers(convArr);
-            page = "<!DOCTYPE html>\n" +
-                    "<html>\n" +
-                    "<body bgcolor=\"#f0f0f0\" align=\"center\" >\n" +
-                    "<h1 align=\"center\"> Result </h1>\n" +
-                    "<h2>Original Matrix</h2>\n" + input ;
-            page += "</body>\n" + "</html>";
+            if(length*length!=inputArr.length) {
+                page = "errorNotSquaredMatrix.html";
+                response.sendRedirect(page);
+            } else {
+                Matrix matrix = new Matrix(length);
+                matrix.SetNumbers(convArr);
+                page = "<!DOCTYPE html>\n" +
+                        "<html>\n" +
+                        "<body bgcolor=\"#f0f0f0\" align=\"center\" >\n" +
+                        "<h1 align=\"center\"> Result </h1>\n" +
+                        "<h2>Original Matrix</h2>\n" + input;
+                page += "</body>\n" + "</html>";
+
+                PrintWriter output = response.getWriter();
+                output.println(page);
+            }
         }
-        response.getWriter().println(page);
     }
 }
